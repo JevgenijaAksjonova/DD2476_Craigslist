a448fb6c5e7f2218c9bcbd77a25dbcf36c5678f1
Magnus Arvidsson <magnus.arvidsson@tvesovla.se>
2017-05-03 22:18:52 +0200

    started work on opening every ad in order to get ad text

---
+++ crawlers/crawlers/spiders/blocket_ad_spider.py
@@ -0,0 +1,64 @@
+import scrapy
+
+
+class BlocketSpider(scrapy.Spider):
+    name = "blocket_mobile"
+    
+    def start_requests(self):
+        urls = [
+#            'https://www.blocket.se/stockholm?q=&cg=5060&w=3&st=s&c=&ca=11&is=1&l=0&md=th',
+            'https://www.blocket.se/stockholm/telefoner_tillbehor/telefoner?cg=5060&w=1&st=s&ca=11&is=1&l=0&md=th&c=5061',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+
+        data_application_name = response.xpath('//body/@data-application-name').extract_first()
+        # if it is the list view
+        if data_application_name == "list_ads":
+            row = response.css('.item_row')[0]
+        
+            try:
+                title_h1 = row.css('h1.media-heading')
+                title = title_h1.css('a ::text').extract_first()
+                url = title_h1.css('a::attr(href)').extract_first()
+                if url is not None:
+                    yield scrapy.Request(url=url, callback=self.parse)
+            except AttributeError:
+                pass
+        elif data_application_name == "view_ad":
+
+            uid = response.url.split('?')[-2].split('.htm')[-2].split('_')[-1]
+            main = response.css('main')
+
+            title = main.css('h1.subject_medium::text').extract_first()
+            seller_info = main.xpath('//ul[@id="seller_info"]/')[0]
+            publish_date = seller_info.xpath('//time/@datetime').extract_first()
+            author = seller_info.xpath('//strong/text()')
+            
+
+
+        for row in response.css('.item_row'):
+            try:
+                title_h1 = row.css('h1.media-heading')
+                title = title_h1.css('a ::text').extract_first()
+                url = title_h1.css('a::attr(href)').extract_first()
+                uid = url.split('?')[-2].split('.htm')[-2].split('_')[-1]
+                price = (row.css('p.list_price ::text').extract_first()).replace(" ", "")[:-2] # Remove whitespaces and :-
+                obj = {
+                        'title': title,
+                        'price': price,
+                        'uid'  : uid,
+                        }
+                print(obj) # This should be served to elasticsearch
+                yield obj
+            except AttributeError:
+                pass
+
+        next_page = response.xpath('//ul[@id="all_pages"]/li[contains(.,"Nästa")]/a/@href').extract_first()
+        print(next_page)
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            print(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
